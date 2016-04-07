a363f8023f8b039ee2719179ba42ddfd3f50f0f9
Michael Sledge <msevilgenius@users.noreply.github.com>
2016-04-07 22:41:20 +0100

    more diagrams

--- doc/diag/svgtopdf.bat
+++ doc/diag/svgtopdf.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Inkscape\inkscape.exe" -D -z --file=%1.svg --export-pdf=..\report\%1.pdf --export-latex
+"C:\Program Files (x86)\Inkscape\inkscape.exe" -D -z --file=%1.svg --export-pdf=..\report\fig\%1.pdf --export-latex
